bc9454fd9e03d0f85e3b42417e40878157479af2
ddvudo <644077730@qq.com>
2019-04-12 08:39:26 +0800

    update

---
+++ src/main/java/CRUD/DAO/CategoryDao.java
@@ -0,0 +1,32 @@
+package CRUD.DAO;
+
+import CRUD.JavaBean.Category;
+import CRUD.JavaBean.CategoryExample;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface CategoryDao {
+    long countByExample(CategoryExample example);
+
+    int deleteByExample(CategoryExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    List<Category> selectByExample(CategoryExample example);
+
+    Category selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Category record, @Param("example") CategoryExample example);
+
+    int updateByExample(@Param("record") Category record, @Param("example") CategoryExample example);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
+}
---
+++ src/main/java/CRUD/JavaBean/Category.java
@@ -0,0 +1,68 @@
+package CRUD.JavaBean;
+
+import java.io.Serializable;
+
+/**
+ * Category
+ *
+ * @author
+ */
+public class Category implements Serializable {
+    private String id;
+
+    private String name;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Category other = (Category) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+                && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("{");
+        sb.append("\"id\":\"")
+                .append(id).append('\"');
+        sb.append(",\"name\":\"")
+                .append(name).append('\"');
+        sb.append('}');
+        return sb.toString();
+    }
+}
